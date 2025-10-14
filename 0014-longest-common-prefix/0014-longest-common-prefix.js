d32c0e9c861fdfbd2cd49ec1a1112285f9109a3b
Yadukrishnan P <169066483+yadukrishnan2004@users.noreply.github.com>
2025-10-14 19:09:09 +0530

    Time: 2 ms (43.02%), Space: 54.9 MB (49.26%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -0,0 +1,29 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+function longestCommonPrefix(strs) {
+  if (strs.length === 0) return "";
+
+
+  for (let i = 0; i < strs[0].length; i++) {
+    const char = strs[0][i];
+
+
+   
+    for (let j = 1; j < strs.length; j++) {
+
+      
+      if (i >= strs[j].length || strs[j][i] !== char) {
+
+
+        return strs[0].slice(0, i);
+      }
+    }
+  }
+
+
+  return strs[0];
+}
+
+    
