7e8490cc389c5689dd5e8a4d50bf9f66d5a3b9db
Yadukrishnan P <169066483+yadukrishnan2004@users.noreply.github.com>
2026-03-04 11:32:55 +0530

    Time: 3 ms (32.14%), Space: 5.1 MB (47.34%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.go
+++ 0125-valid-palindrome/0125-valid-palindrome.go
@@ -1,33 +1,26 @@
 func isPalindrome(s string) bool {
-    if s == " " || len(s)==1 {
-    return true
-}
-    var r strings.Builder
-        result := strings.ReplaceAll(s, " ", "")
-        result = strings.ToLower(result) 
-    	for _,v:=range result {
-            if unicode.IsLetter(v) {
-                r.WriteRune(v)
-            }
-        }
 
-str := r.String()
+	var r strings.Builder
+	result := strings.ToLower(s)
 
+	for _, v := range result {
+		if unicode.IsLetter(v) || unicode.IsDigit(v) {
+			r.WriteRune(v)
+		}
+	}
 
-i := len(str) - 1
-j := 0
+	str := r.String()
 
+	i := len(str) - 1
+	j := 0
 
-if i < 1{
-    return false
-}
-for j < i {
-    if str[j] != str[i] {
-        return false
-    }
-    i--
-    j++
-}
+	for j < i {
+		if str[j] != str[i] {
+			return false
+		}
+		i--
+		j++
+	}
 
 	return true
 }
