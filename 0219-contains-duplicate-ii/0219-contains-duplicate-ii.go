515c618545cf0f9ed10c2e5f0422ef40117ac0b5
Yadukrishnan P <169066483+yadukrishnan2004@users.noreply.github.com>
2026-03-27 17:03:20 +0530

    Time: 0 ms (100%), Space: 12.4 MB (73.07%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.go
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.go
@@ -1,15 +1,35 @@
+func abs(num int)int{
+    if num<0{
+        return num*-1
+    }
+    return num
+}
 func containsNearbyDuplicate(nums []int, k int) bool {
-    index:=make(map[int]int)
+    // index:=make(map[int]int)
 
-    for i,num:=range nums{
-        if pre,ok:=index[num];ok{
-            if i-pre<=k{
+    // for i,num:=range nums{
+    //     if pre,ok:=index[num];ok{
+    //         if i-pre<=k{
+    //             return true
+    //         }
+    //     }
+
+    //     index[num]=i
+
+    // }
+    // return false
+    n:=0
+l:=len(nums)
+    for i:=0;i<l;i++{
+        for j:=i+1;j<l;j++{
+            if nums[i]==nums[j]&& abs(i-j)<=k {
                 return true
             }
+            n++
+            if n>100000{
+                return false
+            }
         }
-
-        index[num]=i
-
     }
     return false
 }
